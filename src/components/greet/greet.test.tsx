1f4b81e5ccda081cbd67ad5c0191bb5e1e56655b
Larry Golding <laurenceg@crosslaketech.com>
2022-08-18 13:10:37 -0700

    Add Greet component and first unit test.

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { text } from "stream/consumers";
+import { Greet } from "./greet";
+
+test("Greet renders correctly", () => {
+  render(<Greet />);
+  const textElement = screen.getByText(/hello/i);
+  expect(textElement).toBeInTheDocument();
+});
