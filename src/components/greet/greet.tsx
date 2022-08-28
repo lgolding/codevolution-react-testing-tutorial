d7998480cfe052d7aa2aef89cc427155093c6654
Larry Golding <laurenceg@crosslaketech.com>
2022-08-28 16:05:53 -0700

    Specify test threshold.

--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -2,5 +2,5 @@ import React, { FC } from "react";
 import { GreetProps } from "./greet.types";
 
 export const Greet: FC<GreetProps> = ({ name }) => {
-  return <div>Hello, {name}</div>;
+  return <div>Hello, {name ? name : "Guest"}</div>;
 };
