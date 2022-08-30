1489dded19895e943a660e566b6902ed7f4a2787
Larry Golding <laurenceg@crosslaketech.com>
2022-08-29 17:17:40 -0700

    Demonstrate level option to getByRole.

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -5,6 +5,18 @@ describe("Application", () => {
   it("renders correctly", () => {
     render(<Application />);
 
+    const pageHeading = screen.getByRole("heading", {
+      name: "Job application form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      name: "Section 1",
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
     const nameElement = screen.getByRole("textbox", {
       name: "Name",
     });
